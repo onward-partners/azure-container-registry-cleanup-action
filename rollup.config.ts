cc9f6986263e8252e673b16fffe39d1ccaa0264c
Sebastian Comans <631504+scomans@users.noreply.github.com>
2026-03-24 10:28:01 +0100

    🐛 FIX: bundling

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,11 +4,12 @@ import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
+
 const config = {
   input: 'src/index.ts',
   output: {
     esModule: true,
-    file: 'dist/index.js',
+    dir: 'dist',
     format: 'es',
     sourcemap: true,
   },
